b1ccb992eb6fdc45333435e493b838135a110877
JamieRez <jamierezendes@gmail.com>
2018-03-10 12:20:28 -0800

    Completed Basic User Auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,32 @@
+const User = require('../models/User');
 module.exports = (app) => {
 
-  app.get('/register')''
+  app.post('/register', (req, res) => {
+    User.findOne({username : req.body.username}, (err, user) => {
+      if(user){
+        res.send({err : "User Already Exists"});
+      }else{
+        let newUser = new User({username : req.body.username});
+        newUser.password = newUser.generateHash(req.body.password);
+        newUser.save((err, user) => {
+          res.send({user: user});
+        });
+      }
+    })
+  });
+
+  app.post('/login', (req, res) => {
+    User.findOne({username : req.body.username}, (err, user) => {
+      if(!user){
+        res.send({err : "No User with username : " + req.body.username});
+      }
+      else if(!user.validPassword(req.body.password)){
+        res.send({err : "Incorrect Password"});
+      }
+      else{
+        res.send({user : user});
+      }
+    })
+  })
 
 }
--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser')
 
 const port = process.env.PORT || '3000';
 
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/happin-local');
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+//========Authentication======
+require('./controllers/auth')(app);
+
+
 
 app.get('/', (req, res) => {
   res.send("Happin");
